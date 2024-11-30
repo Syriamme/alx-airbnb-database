6332f445fbb83a252d066b1f6b0f8b0642f29f88
Syriamme <roridaniel01@gmail.com>
2024-11-30 19:34:30 +0300

    SQL Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -24,4 +24,6 @@ SELECT
 FROM
     User
 FULL OUTER JOIN
-    Booking ON Booking.user_id = User.user_id
+    Booking ON User.user_id = Booking.user_id
+ORDER BY
+    User.user_id, Booking.start_date;
